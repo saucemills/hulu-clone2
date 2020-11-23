adb396479e5b6570bd35263d456dc16909b1fa0a
Jon Miller <millerjw1124@gmail.com>
2020-11-23 15:56:40 -0800

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Header.css";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+
+function Header() {
+    return <div className="header">
+        <div className="header-icons">
+            <div className="header-icon active">
+                <HomeIcon />
+                <p>Home</p>
+            </div>
+
+            <div className="header-icon">
+                <FlashOnIcon />
+                <p>Trending</p>
+            </div>
+
+            <div className="header-icon">
+                <LiveTvIcon />
+                <p>Verified</p>
+            </div>
+            <div className="header-icon">
+                <VideoLibraryIcon />
+                <p>Collections</p>
+            </div>
+            <div className="header-icon">
+                <SearchIcon />
+                <p>Search</p>
+            </div>
+            <div className="header-icon">
+                <PersonOutlineIcon />
+                <p>Account</p>
+            </div>
+        </div>
+        <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png?fit=1280%2C680" alt="hulu logo"></img>
+    </div>;
+}
+
+export default Header
